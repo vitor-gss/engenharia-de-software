14366d8ed8e5e5b445d2a10b41da7c3bd42ad22e
Girleide de Araujo <126255280+vitor-gss@users.noreply.github.com>
2023-06-22 11:51:21 -0300

    Aulas

---
+++ 2° BIM/aula-testes.js
@@ -0,0 +1,15 @@
+const assert = require('assert')
+describe('Usuários', () => {
+    it('O nome não pode ter números', () => {
+        
+    })
+})
+
+/*
+- O nome não pode ter números.
+- Telefone não pode ter String
+- Telefone não pode ter mais que 13, código do país, DDD.
+- Endereço deve ter nome da rua, número, cidade, estado e código postal.
+- Email deve ter nome de usuário, @, e domínio válido; Não pode ter espaço.
+- Senha deve ter entre 6 e 12 caracteres
+*/
